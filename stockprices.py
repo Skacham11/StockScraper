949cb32832451d5b5242a39c64dca8bee30ae689
Skacham11 <66738929+Skacham11@users.noreply.github.com>
2024-06-15 14:35:14 -0400

    Addedweb scraping functionality with BeautifulSoup
    - Implemented HTTP GET request using the `requests` library
    - Parsed HTML content using `BeautifulSoup`
    - Set up the structure for further data extraction and processing

--- stockprices.py
+++ stockprices.py
@@ -4,3 +4,5 @@ import requests
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
 url = 'https://finance.yahoo.com/quote/AAPL/'
 
+r = requests.get(url, headers=headers)
+soup = BeautifulSoup(r.text, 'html.parser')
