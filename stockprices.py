b68ce9d75595b08482cba18ca2bb1ad2a783092a
Skacham11 <66738929+Skacham11@users.noreply.github.com>
2024-06-15 14:28:20 -0400

    Setting up project
    Added imports
    Added headers
    Added url

---
+++ stockprices.py
@@ -0,0 +1,6 @@
+from bs4 import BeautifulSoup
+import requests
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+url = 'https://finance.yahoo.com/quote/AAPL/'
+
