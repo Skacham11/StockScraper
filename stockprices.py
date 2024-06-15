a636aee7922d10155ea8a53985f585a7c0842816
Skacham11 <66738929+Skacham11@users.noreply.github.com>
2024-06-15 15:10:59 -0400

    Extracted volume, 1y target Est, 52 Week Range

--- stockprices.py
+++ stockprices.py
@@ -3,11 +3,32 @@ import requests
 
 
 def getData(stock_ticker):
-    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) '
+                      'AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'
+    }
     url = f'https://finance.yahoo.com/quote/{stock_ticker}/'
     r = requests.get(url, headers=headers)
     soup = BeautifulSoup(r.text, 'html.parser')
-    price = soup.find('div', {'class': 'container svelte-mgkamr'}).find_all('span')[0].text
-    price_change_dollars = soup.find('div', {'class': 'container svelte-mgkamr'}).find_all('span')[1].text
-    price_change_percentage = soup.find('div', {'class': 'container svelte-mgkamr'}).find_all('span')[2].text
-    print(price, price_change_dollars, price_change_percentage)
+
+    quote_stats_div = soup.find('div', {'data-testid': 'quote-statistics'})
+
+    data = {}
+
+    for li in quote_stats_div.find_all('li', class_='last-sm last-lg svelte-tx3nkj'):
+        label_span = li.find('span', class_='label svelte-tx3nkj')
+        value_span = li.find('span', class_='value svelte-tx3nkj')
+
+        if label_span and value_span:
+            label = label_span.get_text(strip=True)
+            value = value_span.get_text(strip=True)
+            data[label] = value
+
+    fifty_two_week_range = soup.find('li', class_='last-md svelte-tx3nkj').find('span', class_='value svelte-tx3nkj')
+    if fifty_two_week_range:
+        data['52 Week Range'] = fifty_two_week_range.get_text(strip=True)
+
+    print(data)
+
+# Example usage:
+getData('AAPL')
