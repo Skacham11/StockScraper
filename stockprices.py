d2a3c723b462dd494f1b4ac273ca38c1da223801
Skacham11 <66738929+Skacham11@users.noreply.github.com>
2024-06-15 14:40:16 -0400

    Added getData function
    Created getData function that accepts a stock ticker as input. Cleaned up code.

--- stockprices.py
+++ stockprices.py
@@ -1,13 +1,13 @@
 from bs4 import BeautifulSoup
 import requests
 
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
-url = 'https://finance.yahoo.com/quote/AAPL/'
 
-r = requests.get(url, headers=headers)
-soup = BeautifulSoup(r.text, 'html.parser')
-
-price = soup.find('div', {'class': 'container svelte-mgkamr'}).find_all('span')[0].text
-price_change_dollars = soup.find('div', {'class': 'container svelte-mgkamr'}).find_all('span')[1].text
-price_change_percentage = soup.find('div', {'class': 'container svelte-mgkamr'}).find_all('span')[2].text
-print(price, price_change_dollars, price_change_percentage)
+def getData(stock_ticker):
+    headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36'}
+    url = f'https://finance.yahoo.com/quote/{stock_ticker}/'
+    r = requests.get(url, headers=headers)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    price = soup.find('div', {'class': 'container svelte-mgkamr'}).find_all('span')[0].text
+    price_change_dollars = soup.find('div', {'class': 'container svelte-mgkamr'}).find_all('span')[1].text
+    price_change_percentage = soup.find('div', {'class': 'container svelte-mgkamr'}).find_all('span')[2].text
+    print(price, price_change_dollars, price_change_percentage)
