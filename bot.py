f9792f6af9cdcf02c9fef0df04343a8908ca73e5
Skacham11 <66738929+Skacham11@users.noreply.github.com>
2024-06-16 12:29:01 -0400

    Created .env file to hide discord token

--- bot.py
+++ bot.py
@@ -2,6 +2,14 @@ import discord
 from discord.ext import commands
 import stockprices 
 import io
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+load_dotenv()
+
+# Define your Discord bot token
+TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.message_content = True
